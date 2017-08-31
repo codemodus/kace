92f8d1bc21c4c533ec478c5f799994ca5935a073
Daved <daved@codemodus.com>
2017-08-31 14:37:24 -0700

    Add functional tests for kace methods.

--- kace_func_test.go
+++ kace_func_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/codemodus/kace"
 )
 
+var (
+	altCIMap = map[string]bool{
+		"THIS": true,
+	}
+)
+
 func TestFuncPascal(t *testing.T) {
 	var data = []struct {
 		i string
@@ -108,6 +114,126 @@ func TestFuncKebabUpper(t *testing.T) {
 	for _, v := range data {
 		want := v.o
 		got := kace.KebabUpper(v.i)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestFuncKacePascal(t *testing.T) {
+	var data = []struct {
+		i string
+		o string
+	}{
+		{"This is a test sql", "THISIsATestSql"},
+		{"this is a test sql", "THISIsATestSql"},
+	}
+
+	k, _ := kace.New(altCIMap)
+
+	for _, v := range data {
+		want := v.o
+		got := k.Pascal(v.i)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestFuncKaceCamel(t *testing.T) {
+	var data = []struct {
+		i string
+		o string
+	}{
+		{"this is a test sql", "thisIsATestSql"},
+		{"this_is_a_test sql", "thisIsATestSql"},
+	}
+
+	k, _ := kace.New(altCIMap)
+
+	for _, v := range data {
+		want := v.o
+		got := k.Camel(v.i)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestFuncKaceSnake(t *testing.T) {
+	var data = []struct {
+		i string
+		o string
+	}{
+		{"thisIsATestSQL", "this_is_a_test_sql"},
+		{"ThisIsATestSQL", "this_is_a_test_sql"},
+	}
+
+	k, _ := kace.New(altCIMap)
+
+	for _, v := range data {
+		want := v.o
+		got := k.Snake(v.i)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestFuncKaceSnakeUpper(t *testing.T) {
+	var data = []struct {
+		i string
+		o string
+	}{
+		{"thisIsATestSQL", "THIS_IS_A_TEST_SQL"},
+		{"ThisIsATestSQL", "THIS_IS_A_TEST_SQL"},
+	}
+
+	k, _ := kace.New(altCIMap)
+
+	for _, v := range data {
+		want := v.o
+		got := k.SnakeUpper(v.i)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestFuncKaceKebab(t *testing.T) {
+	var data = []struct {
+		i string
+		o string
+	}{
+		{"thisIsATestSQL", "this-is-a-test-sql"},
+		{"ThisIsATestSQL", "this-is-a-test-sql"},
+	}
+
+	k, _ := kace.New(altCIMap)
+
+	for _, v := range data {
+		want := v.o
+		got := k.Kebab(v.i)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestFuncKaceKebabUpper(t *testing.T) {
+	var data = []struct {
+		i string
+		o string
+	}{
+		{"thisIsATestSQL", "THIS-IS-A-TEST-SQL"},
+		{"ThisIsATestSQL", "THIS-IS-A-TEST-SQL"},
+	}
+
+	k, _ := kace.New(altCIMap)
+
+	for _, v := range data {
+		want := v.o
+		got := k.KebabUpper(v.i)
 		if got != want {
 			t.Errorf("got %v, want %v", got, want)
 		}
